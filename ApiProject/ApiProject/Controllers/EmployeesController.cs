a1c11768e326edd96a200789151890dd708587d6
jit226 <jitendra.mca13@gmail.com>
2019-02-07 22:34:31 +0530

    First commit

---
+++ ApiProject/ApiProject/Controllers/EmployeesController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
+
+namespace ApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    //[DisableCors]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        
+
+        private readonly ApiProjectContext _context;
+
+        public EmployeesController(ApiProjectContext context)
+        {
+            _context = context;
+
+            //if (_context.TodoItems.Count() == 0)
+            //{
+            //    // Create a new TodoItem if collection is empty,
+            //    // which means you can't delete all TodoItems.
+            //    _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+            //    _context.SaveChanges();
+            //}
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            return _context.Employees;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        
+        [HttpPost]
+        public void Post([FromBody] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+            }
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == employee.Id)
+                {
+                    _context.Entry(employee).State = EntityState.Modified;
+                }
+            }
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                
+            }    
+        }
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            if (employee != null)
+            {
+                _context.Employees.Remove(employee);
+                _context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ ApiProject/ApiProject/Models/ApiProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProject.Models
+{
+    public class ApiProjectContext : DbContext
+    {
+        public ApiProjectContext(DbContextOptions<ApiProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
