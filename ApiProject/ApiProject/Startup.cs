b8b299e9fdd4f65e74e47f3a2a349a5005b374f0
jit226 <41953253+jit226@users.noreply.github.com>
2019-02-08 11:35:57 +0530

    Just comment line add

--- ApiProject/ApiProject/Startup.cs
+++ ApiProject/ApiProject/Startup.cs
@@ -36,6 +36,7 @@ namespace ApiProject
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApiProjectContext context)
         {
+            //If Database not exist then create database 
             context.Database.EnsureCreated();
 
             if (env.IsDevelopment())
